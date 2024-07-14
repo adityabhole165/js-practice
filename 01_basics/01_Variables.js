3376c361925d766340a861b72128d09332a72301
Aditya Bhole <130380292+adityabhole165@users.noreply.github.com>
2024-07-14 05:21:31 +0000

    01_Variables

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,16 @@
+const accountId = 144553
+let accuntEmail = "adi@gmail.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+let accountstate;
+
+// accountId = 2 
+
+accuntEmail = "ab@gmail.com"
+accountPassword ="000"
+accountCity="pune"
+
+
+console.log(accountId);
+
+console.table([accountId,accuntEmail,accountPassword,accountCity,accountstate])
